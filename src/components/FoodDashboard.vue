697bdc7fc9a1c53f87356202c48bf1e8408cfef3
paulopariz <paulopariz01@gmail.com>
2023-02-06 17:34:58 -0300

    delete

--- src/components/FoodDashboard.vue
+++ src/components/FoodDashboard.vue
@@ -35,7 +35,7 @@
               {{ s.tipo }}
             </option>
           </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
     </div>
@@ -62,8 +62,6 @@ export default {
 
       this.burgers = data;
 
-      console.log(data);
-
       this.getStatus();
     },
 
@@ -73,6 +71,18 @@ export default {
       const data = await api.json();
 
       this.status = data;
+    },
+
+    async deleteBurger(id) {
+      const api = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
+
+      const res = await api.json();
+
+      console.log(res);
+
+      this.getPedidos();
     },
   },
 
