b6dc2d702d9f7baea814fc5303a96f2147f4f4ba
paulopariz <paulopariz01@gmail.com>
2023-02-06 16:34:34 -0300

    component dashboard initial

--- src/views/RequestsView.vue
+++ src/views/RequestsView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>PEDIDOS</h1>
+    <FoodDashboard />
   </div>
 </template>
+
+<script>
+import FoodDashboard from "@/components/FoodDashboard.vue";
+export default {
+  components: { FoodDashboard },
+  name: "RequestsView",
+};
+</script>
