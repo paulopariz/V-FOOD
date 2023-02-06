7da736e2a13a55497d277a7c080a3e747b80d1db
paulopariz <paulopariz01@gmail.com>
2023-02-06 17:44:44 -0300

    update status

--- src/components/FoodDashboard.vue
+++ src/components/FoodDashboard.vue
@@ -24,7 +24,7 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
+          <select name="status" class="status" @change="updatedBurger($event, burger.id)">
             <option value="">Selecione o status</option>
             <option
               v-for="s in status"
@@ -83,6 +83,22 @@ export default {
       console.log(res);
 
       this.getPedidos();
+    },
+
+    async updatedBurger(event, id) {
+      const option = event.target.value;
+
+      const dataJson = JSON.stringify({ status: option });
+
+      const api = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      });
+
+      const res = await api.json();
+
+      console.log(res);
     },
   },
 
