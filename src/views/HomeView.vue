322c79c1b2c368588dfedffe8bbcc7d5b0de04e4
paulopariz <paulopariz01@gmail.com>
2023-02-07 20:09:05 -0300

    component menubtn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
     <FoodBanner />
+    <FoodMenuBtn />
   </div>
 </template>
 
 <script>
 import FoodBanner from "@/components/FoodBanner.vue";
+import FoodMenuBtn from "@/components/FoodMenuBtn.vue";
 export default {
-  components: { FoodBanner },
+  components: { FoodBanner, FoodMenuBtn },
 };
 </script>
 
